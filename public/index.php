79783cd2fdede94b88d164b5751a9b4897d93a6b
chengmingqiu <1226211879@qq.com>
2020-03-02 22:01:58 +0800

    s

--- public/index.php
+++ public/index.php
@@ -13,6 +13,8 @@
 // 定义应用目录
 
 header("Access-Control-Allow-Origin:*");
+header('Access-Control-Allow-Headers:*');
+header('Access-Control-Allow-Methods:*');
 header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
 
