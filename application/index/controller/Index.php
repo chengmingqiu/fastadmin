2011167dc176908198e0245bc64179506a006d4f
chengmingqiu <ChingMQ_Charles@163.com>
2020-03-04 16:04:54 +0800

    s

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -13,17 +13,34 @@ class Index extends Frontend
 
     public function index()
     {
-        if (isset ($_SERVER['HTTP_X_WAP_PROFILE'])){
-            return $this->view->fetch('isapp');
-        }
-        if (isset ($_SERVER['HTTP_VIA']) && stristr($_SERVER['HTTP_VIA'], "wap")) {
-            return $this->view->fetch('isapp');
-        }
         if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false ) {
             return $this->view->fetch('isapp');
         }
-        return $this->view->fetch('ispc');
-
+        $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+        $is_pc = (strpos($agent, 'windows nt')) ? true : false;
+        $is_mac = (strpos($agent, 'mac os')) ? true : false;
+        $is_iphone = (strpos($agent, 'iphone')) ? true : false;
+        $is_android = (strpos($agent, 'android')) ? true : false;
+        $is_ipad = (strpos($agent, 'ipad')) ? true : false;
+        if($is_pc  == true){
+             return $this->view->fetch('ispc');
+        }
+        
+        if($is_mac  == true){
+              return $this->view->fetch('ispc');
+        }
+        
+        if($is_iphone  == true){
+              return $this->view->fetch('isapp');
+        }
+        
+        if($is_android == true){
+              return $this->view->fetch('isapp');
+        }
+        
+        if($is_ipad == true){
+              return $this->view->fetch('ispc');
+        }
     }
 
     public function news()
